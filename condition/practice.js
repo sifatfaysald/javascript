3bf78c2e5a5ea52121d41a52f41489f353233e79
sifatfaysald <sifatfaysald@gmail.com>
2025-07-15 01:04:59 +0600

    js condition practice

---
+++ condition/practice.js
@@ -0,0 +1,57 @@
+// age validator
+let age = 16;
+
+if (age > 18){
+    console.log('you are an adult');
+} else {
+    console.log('you are minor');
+}
+
+
+// grade calculation:
+let marks = -99;
+
+if(marks < 0){
+    console.log(`Invalid Entry!!`)
+}
+else if(marks >= 90){
+    console.log(`Grade A+ [${marks}]`);
+} else if (marks >= 89){
+    console.log(`Grade B+ [${marks}]`);
+} else if (marks >= 59){
+    console.log(`Grade C+ [${marks}]`);
+} else {
+    console.log(`Fail [${marks}]`);
+}
+
+// name game
+let name = 'Sifat';
+if (name === 'Sifat'){
+    console.log(`Welcome Back ${name}`);
+} else {
+    console.log('Hello Stranger');
+}
+
+// Time gritting
+let hour = 14;
+
+if (hour <0){
+    console.log('invalid entry')
+}
+else if(hour >= 0 && hour <= 11){
+    console.log(`Good Morning [${hour}]`)
+} else if (hour >= 12 && hour <= 17){
+    console.log(`Good Afternoon [${hour}]`)
+} else if (hour >= 18 && hour <= 23) {
+    console.log(`Good Evening [${hour}]`)
+} else {
+    console.log('good night')
+}
+
+// even odd
+let num = 11;
+if (num % 2 === 0){
+    console.log('even')
+} else {
+    console.log('odd');
+}
